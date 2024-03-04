2c9cb369aab7df21284b3eb8d2eb2fe352136ad2
Dennis Peter <peterdennis573@gmail.com>
2024-03-04 22:47:29 +0300

    Worked on the title component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Program from "./components/Programs/Program";
+import Title from "./components/Title/Title";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className="container">
+        <Title subTitle='Our PROGRAM' title='What We Offer'/>
         <Program />
       </div>
     </div>
